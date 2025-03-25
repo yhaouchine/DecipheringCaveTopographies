2f8240d9f0dff4de5fe5320739708df59d924aab
Yanis Haouchine <yanis.haouchine4@etu.univ-lorraine.fr>
2025-03-25 15:33:05 +0100

    Adding area and perimeter computing + optimizations

--- polygones_test.py
+++ polygones_test.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 import open3d as o3d
 from contour_extractor import ContourExtractor
 from process_cloud import PointCloudProcessor
+from typing import Tuple
 
 
 class Shape:
-    def __init__(self, nb_points, color = (0, 0, 0)):
-        self.color = color
+    def __init__(self, nb_points, default_color=(0, 0, 0)):
+        self.nb_points = nb_points
+        self.color = default_color
         self.pc = None
         self.points = []
         self.x = []
@@ -27,24 +29,27 @@ class Shape:
     def save_ply(self, filename):
         o3d.io.write_point_cloud(filename, self.pc)
 
-
 class Sphere(Shape):
     def __init__(self, radius, nb_points, color=(0, 0, 1)):
         super().__init__(nb_points, color)
         self.nb_points = nb_points
         self.center = (0, 0, 0)
         self.radius = radius
-        self.real_area = np.pi * (self.radius ** 2)
-        self.real_perimeter = 2 * np.pi * self.radius
+        self.disk_area = np.pi * (self.radius ** 2)
+        self.disk_perimeter = 2 * np.pi * self.radius
         self.disk_pc = None
 
         self.generate()
         self.pc = self.to_point_cloud()
+        print("===== Disk =====")
+        print(f"Disk area = {self.disk_area:.4f}")
+        print(f"Disk perimeter = {self.disk_perimeter:.4f}")
+        print()
 
     def generate(self):
         phi = np.random.uniform(0, np.pi, self.nb_points)
         theta = np.random.uniform(0, 2 * np.pi, self.nb_points)
-        r = np.random.uniform(0, self.radius, self.nb_points) ** (1/3)  # Cube root for uniform distribution in volume
+        r = np.cbrt(np.random.uniform(0, self.radius, self.nb_points))  # Cube root for uniform distribution in volume
 
         x = self.center[0] + r * np.sin(phi) * np.cos(theta)
         y = self.center[1] + r * np.sin(phi) * np.sin(theta)
@@ -52,10 +57,9 @@ class Sphere(Shape):
 
         self.points = np.column_stack((x, y, z))
     
-    def extract_disk(self):
+    def extract_disk(self, tolerance=0.01):
         # Extract points close to the center of the sphere (z = 0)
         z_level = 0
-        tolerance = 0.005
         disk_points = self.points[np.abs(self.points[:, 2] - z_level) < tolerance]
         
         # Create a new point cloud for the disk
@@ -66,20 +70,26 @@ class Sphere(Shape):
         return self.disk_pc
 
 class Cuboid(Shape):
-    def __init__(self, nb_points, min_corner=(-2, -1, -1), max_corner=(2, 1, 1), color=(1, 0, 0)):
+    def __init__(self, nb_points, lengths=(4, 2, 3), center=(0, 0, 0), color=(1, 0, 0)):
         super().__init__(nb_points, color)
-        self.min_corner = np.array(min_corner)
-        self.max_corner = np.array(max_corner)
+        self.lengths = np.array(lengths)
+        self.center = np.array(center)
         self.nb_points = nb_points
-        self.real_area = None
-        self.real_perimeter = None
-        self.parallelogram_pc = None
+        self.rectangle_area = self.lengths[0] * self.lengths[1]
+        self.rectangle_perimeter = 2 * (self.lengths[0] + self.lengths[1])
+        self.rectangle_pc = None
+
         self.generate()
         self.pc = self.to_point_cloud()
-    
+        print("===== Rectangle =====")
+        print(f"Rectangle area = {self.rectangle_area:.4f}")
+        print(f"Rectangle perimeter =  {self.rectangle_perimeter:.4f}")
+        print()
+
     def generate(self):
-        x_min, y_min, z_min = self.min_corner
-        x_max, y_max, z_max = self.max_corner
+        half_lengths = self.lengths / 2
+        x_min, y_min, z_min = self.center - half_lengths
+        x_max, y_max, z_max = self.center + half_lengths
 
         x = np.random.uniform(x_min, x_max, self.nb_points)
         y = np.random.uniform(y_min, y_max, self.nb_points)
@@ -90,14 +100,14 @@ class Cuboid(Shape):
     def extract_section(self):
         z_level = 0
         tolerance = 0.01
-        parallelogram_points = self.points[np.abs(self.points[:, 2] - z_level) < tolerance]
-        
+        rectangle_points = self.points[np.abs(self.points[:, 2] - z_level) < tolerance]
+
         # Create a new point cloud for the section
-        self.parallelogram_pc = o3d.geometry.PointCloud()
-        self.parallelogram_pc.points = o3d.utility.Vector3dVector(parallelogram_points)
-        self.parallelogram_pc.colors = o3d.utility.Vector3dVector(np.tile(self.color, (len(parallelogram_points), 1)))
-        
-        return self.parallelogram_pc
+        self.rectangle_pc = o3d.geometry.PointCloud()
+        self.rectangle_pc.points = o3d.utility.Vector3dVector(rectangle_points)
+        self.rectangle_pc.colors = o3d.utility.Vector3dVector(np.tile(self.color, (len(rectangle_points), 1)))
+
+        return self.rectangle_pc
 
 class Pyramid(Shape):
     def __init__(self, nb_points, base_size=4, height=5, center=(0, 0, 0), color=(0, 1, 0)):
@@ -105,11 +115,17 @@ class Pyramid(Shape):
         self.base_size = base_size
         self.height = height
         self.nb_points = nb_points
-        self.real_area = None
-        self.real_perimeter = None
+        self.real_area = 0.5 * self.base_size * self.height
+        self.real_perimeter = self.base_size + 2 * np.sqrt((self.base_size / 2) ** 2 + self.height ** 2)
         self.center = np.array(center)
+        self.triangle_pc = None
+
         self.generate()
         self.pc = self.to_point_cloud()
+        print("===== Triangle =====")
+        print(f"Triangle area = {self.real_area: .4f}")
+        print(f"Triangle perimeter = {self.real_perimeter: .4f}")
+        print()
 
     def generate(self):
         cx, cy, cz = self.center
@@ -124,60 +140,102 @@ class Pyramid(Shape):
         ]
         v_top = (cx, cy, cz + self.height)
 
-        base_points_count = self.nb_points // 2
-        side_points_count = self.nb_points // 2
+        # Generate random points inside the pyramid
+        points = []
+        for _ in range(self.nb_points):
+            # Generate a random height level with linear distribution
+            h = np.random.uniform(0, 1)
+            z = cz + h * self.height
 
-        # Generate the base points
-        x = np.random.uniform(cx - half_size, cx + half_size, base_points_count)
-        y = np.random.uniform(cy - half_size, cy + half_size, base_points_count)
-        z = np.full_like(x, cz)
-        base_points = np.column_stack((x, y, z))
+            # Compute the size of the base at this height
+            scale = 1 - h
+            base_half_size = half_size * scale
 
-        # Generate the faces points
-        side_points = []
-        for i in range(4):
-            p1, p2 = v_base[i], v_base[(i + 1) % 4]
-            for _ in range(side_points_count // 4):
-                t1, t2 = np.random.uniform(0, 1, 2)
-                if t1 + t2 > 1:
-                    t1, t2 = 1 - t1, 1 - t2
-                point = (1 - t1 - t2) * np.array(p1) + t1 * np.array(p2) + t2 * np.array(v_top)
-                side_points.append(point)
+            # Generate a random point within the scaled base
+            x = np.random.uniform(cx - base_half_size, cx + base_half_size)
+            y = np.random.uniform(cy - base_half_size, cy + base_half_size)
 
-        side_points = np.array(side_points)
-        self.points = np.vstack((base_points, side_points))
+            points.append((x, y, z))
 
+        self.points = np.array(points)
+
+    def extract_section(self):
+        x_level = 5.0
+        tolerance = 0.01
+        triangle_points = self.points[np.abs(self.points[:, 0] - x_level) < tolerance]
+
+        self.triangle_pc = o3d.geometry.PointCloud()
+        self.triangle_pc.points = o3d.utility.Vector3dVector(triangle_points)
+        self.triangle_pc.colors = o3d.utility.Vector3dVector(np.tile(self.color, (len(triangle_points), 1)))
+
+        return self.triangle_pc
+    
+def generate_save_clouds(
+    num_points: int, 
+    sphere_radius: float, 
+    save_folder: str, 
+    save: bool = False, 
+    s: bool = True,
+    c: bool = True,
+    p: bool = True
+    ) -> Tuple[Sphere, Cuboid, Pyramid]:
+    
+    # Generating shapes
+    sphere_instance = Sphere(radius=sphere_radius, nb_points=num_points, color=(0, 0, 1)) if s else None
+    cuboid_instance = Cuboid(nb_points=num_points, lengths=(4, 2, 3), color=(1, 0, 0)) if c else None
+    pyramid_instance = Pyramid(nb_points=num_points, base_size=4, height=5, center=(5, 5, 0), color=(0, 1, 0)) if p else None
+
+    if save:
+        try:
+            save_folder = Path(save_folder) if not isinstance(save_folder, Path) else save_folder
+            if s:
+                sphere_instance.save_ply(save_folder / "sphere.ply")
+            if c:
+                cuboid_instance.save_ply(save_folder / "cuboid.ply")
+            if p:
+                pyramid_instance.save_ply(save_folder / "pyramid.ply")
+
+        except Exception as e:
+            import traceback
+            print(f"An error occurred while saving PLY files: {e}")
+            traceback.print_exc()
+
+    return sphere_instance, cuboid_instance, pyramid_instance
 
 if __name__ == "__main__":
-        n_points = 2000000  # Reduced number of points for testing
+        
         save_folder = Path("test_shapes")
         save_folder.mkdir(parents=True, exist_ok=True)
 
-        #sphere_instance = Sphere(radius=1, nb_points=n_points, color=(0, 0, 1))
-        cuboid_instance = Cuboid(nb_points=n_points, min_corner=(-2, -1, -1), max_corner=(2, 1, 1), color=(1, 0, 0))
-        #pyramid_instance = Pyramid(nb_points=n_points, base_size=4, height=5, center=(5, 5, 0), color=(0, 1, 0))
-
-        # # Saving files
-        # try:
-        #     sphere_instance.save_ply(save_folder / "sphere.ply")
-        #     cuboid_instance.save_ply(save_folder / "cuboid.ply")
-        #     pyramid_instance.save_ply(save_folder / "pyramid.ply")
-        #     print("PLY files saved.")
-        # except Exception as e:
-        #     print(f"An error occurred while saving PLY files: {e}")
+        sphere_instance, cuboid_instance, pyramid_instance = generate_save_clouds(
+            num_points=20000, 
+            sphere_radius=1, 
+            save=False, 
+            save_folder=save_folder, 
+            s=True, 
+            c=True, 
+            p=True
+            )
         
+        # ======= Generate disk =======
         # disk_pc = sphere_instance.extract_disk()
         # disk_processor = PointCloudProcessor(sphere_instance.pc)
         # disk_processor.visualizer(window_name="Disk", geom1=disk_pc, save=False)
 
-        parallelogram_pc = cuboid_instance.extract_section()
-        parallelogram_processor = PointCloudProcessor(cuboid_instance.pc)
-        parallelogram_processor.visualizer(window_name="Parallelogram", geom1=parallelogram_pc, save=False)
+        # ======= Generate rectangle =======
+        # parallelogram_pc = cuboid_instance.extract_section()
+        # parallelogram_processor = PointCloudProcessor(cuboid_instance.pc)
+        # parallelogram_processor.visualizer(window_name="Parallelogram", geom1=parallelogram_pc, save=False)
 
-        cloud = ContourExtractor()
-        cloud.load_cloud(pc_name="parallelogram.ply", parent_folder=save_folder)
-        cloud.downsample(voxel_size=0.01)
-        cloud.pca_projection()
-        cloud.extract(concavity=1.0, length_threshold=0.2)
+        # ======= Generate triangle =======
+        # triangle_pc = pyramid_instance.extract_section()
+        # triangle_processor = PointCloudProcessor(pyramid_instance.pc)
+        # triangle_processor.visualizer(window_name="Triangle", geom1=triangle_pc, save=False)
+
+        # cloud = ContourExtractor()
+        # cloud.load_cloud(pc_name="triangle.ply", parent_folder=save_folder)
+        # cloud.downsample(voxel_size=0.01)
+        # cloud.pca_projection()
+        # cloud.extract(concavity=1.0, length_threshold=0.2)
 
     
